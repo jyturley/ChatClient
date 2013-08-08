fe5f9267573cfa2e491cc7da9e9a037c01da0274
jyt <jyturley@berkeley.edu>
2013-08-08 15:30:01 -0700

    created xcode file

---
+++ ChatClient/ChatClient/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ChatClient
+//
+//  Created by James Turley on 8/8/13.
+//
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
